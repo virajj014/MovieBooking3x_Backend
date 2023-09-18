fbe8b9550712167f258d45a219ed846aea64107a
Viraj Jain <76771864+virajj014@users.noreply.github.com>
2023-09-18 20:46:46 +0530

    day1

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 8000;
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+require('./db')
+
+
+app.use(bodyParser.json());
+const allowedOrigins = ['http://localhost:3000']; // Add more origins as needed
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        credentials: true, // Allow credentials
+    })
+);
+app.use(cookieParser());
+
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'The API is working' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
